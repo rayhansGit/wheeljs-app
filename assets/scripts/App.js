05aafedabd9bd28edffcf840f2221e2ab7e49ae1
Md Mahbub Alam Khan <md.m.khan@tuni.fi>
2022-08-08 16:33:46 +0300

    added user activity and api communication feature.

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -1,5 +1,12 @@
 import "../styles/css_grid/simple-grid.scss"
 import "../styles/style.scss"
+import GlobalActivity from "./modules/globalActivity"
+
+var appGlobalActivity = new GlobalActivity()
+var name = appGlobalActivity.get_my_name()
+// console.log("Name: " + name)
+
+// console.log("Game id" + )
 
 import PlayAudio from "./modules/playAudio"
 import UserActivity from "./modules/userActivity"
@@ -8,10 +15,11 @@ import AppPopUp from "./modules/appPopup"
 import GameConfig from "./GameConfig"
 
 var gameConfig = new GameConfig()
-var userActivity = new UserActivity()
+//var userActivity = new UserActivity()
 // var audio = new PlayAudio()
 // console.log(userActivity)
 new AppPopUp()
+
 if (module.hot) {
   module.hot.accept()
 }
---
+++ assets/scripts/modules/globalActivity.js
@@ -0,0 +1,15 @@
+class GlobalActivity {
+  constructor() {
+    this.events()
+  }
+
+  events() {
+    //console.log("hello from global activity")
+  }
+
+  get_my_name() {
+    //return "mahbub"
+  }
+}
+
+export default GlobalActivity
